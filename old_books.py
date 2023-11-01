ae46fb2e0245a55b516ac38e08777d20afda4115
Martin <125393209+DWarmech@users.noreply.github.com>
2023-11-02 00:21:42 +0200

    Update old_books.py

--- old_books.py
+++ old_books.py
@@ -1,1 +1,18 @@
 
+book = input()
+book_count = 0
+book_found = False
+
+current_book = input()
+while current_book != "No More Books":
+    if current_book == book:
+        book_found = True
+        print(f"You checked {book_count} books and found it.")
+        break
+
+    book_count += 1
+    current_book = input()
+
+if not book_found:
+    print("The book you search is not here!")
+    print(f"You checked {book_count} books.")
